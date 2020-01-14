f33a1b5f8069bd759abfee802c98ba8b5fc081dc
Michelle Cupp <shellcupp@gmail.com>
2020-01-14 14:46:02 -0600

    updates

--- js/index.js
+++ js/index.js
@@ -1,1 +1,28 @@
 // Your code goes here
+const h1 = document.querySelector('h1');
+h1.addEventListener('mouseenter', () => {
+h1.style.color ='yellow'
+h1.style.fontWeight ='bold'
+h1.style.textDecoration='underline'
+});
+
+const navlinks = document.querySelectorAll('.nav-link');
+navlinks.forEach( navlinks => {
+    navlinks.addEventListener('click', () => { navlinks.style.color ='yellow'
+})
+ });
+
+ const navbackground = document.querySelector('.main-navigation');
+
+ navbackground.addEventListener('mouseenter', () => {
+     navbackground.style.backgroundColor = 'black'
+ })
+ navbackground.addEventListener('mouseleave', () =>{
+     navbackground.style.backgroundColor = 'gray'
+ });
+
+ const buspic = document.querySelector('.intro img');
+ buspic.addEventListener('dblclick', () => {
+    buspic.src = 'img\bus.jpg'
+    buspic.alt ='new bus'
+ });
